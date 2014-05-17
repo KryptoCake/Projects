798901b6385b12428efa598489cec488dc6197f1
Alex Try <tryescas@gmail.com>
2014-05-17 12:09:37 -0600

    excel Addin
    esta es la primera subida

---
+++ ExcelAddIn1/ExcelAddIn1/Ribbon1.Designer.vb
@@ -0,0 +1,92 @@
+﻿Partial Class Ribbon1
+    Inherits Microsoft.Office.Tools.Ribbon.RibbonBase
+
+    <System.Diagnostics.DebuggerNonUserCode()> _
+   Public Sub New(ByVal container As System.ComponentModel.IContainer)
+        MyClass.New()
+
+        'Requerido para la compatibilidad con el Diseñador de composiciones de clases Windows.Forms
+        If (container IsNot Nothing) Then
+            container.Add(Me)
+        End If
+
+    End Sub
+
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Public Sub New()
+        MyBase.New(Globals.Factory.GetRibbonFactory())
+
+        'El Diseñador de componentes requiere esta llamada.
+        InitializeComponent()
+
+    End Sub
+
+    'Component invalida a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de componentes
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de componentes requiere el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de componentes.
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim RibbonDialogLauncherImpl1 As Microsoft.Office.Tools.Ribbon.RibbonDialogLauncher = Me.Factory.CreateRibbonDialogLauncher
+        Me.Tab1 = Me.Factory.CreateRibbonTab
+        Me.Group1 = Me.Factory.CreateRibbonGroup
+        Me.Group2 = Me.Factory.CreateRibbonGroup
+        Me.Tab1.SuspendLayout()
+        '
+        'Tab1
+        '
+        Me.Tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office
+        Me.Tab1.Groups.Add(Me.Group1)
+        Me.Tab1.Groups.Add(Me.Group2)
+        Me.Tab1.Label = "TabAddIns"
+        Me.Tab1.Name = "Tab1"
+        '
+        'Group1
+        '
+        Me.Group1.DialogLauncher = RibbonDialogLauncherImpl1
+        Me.Group1.Label = "Group1"
+        Me.Group1.Name = "Group1"
+        '
+        'Group2
+        '
+        Me.Group2.Label = "Group2"
+        Me.Group2.Name = "Group2"
+        '
+        'Ribbon1
+        '
+        Me.Name = "Ribbon1"
+        Me.RibbonType = "Microsoft.Excel.Workbook"
+        Me.Tabs.Add(Me.Tab1)
+        Me.Tab1.ResumeLayout(False)
+        Me.Tab1.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Tab1 As Microsoft.Office.Tools.Ribbon.RibbonTab
+    Friend WithEvents Group1 As Microsoft.Office.Tools.Ribbon.RibbonGroup
+    Friend WithEvents Group2 As Microsoft.Office.Tools.Ribbon.RibbonGroup
+End Class
+
+Partial Class ThisRibbonCollection
+
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Friend ReadOnly Property Ribbon1() As Ribbon1
+        Get
+            Return Me.GetRibbon(Of Ribbon1)()
+        End Get
+    End Property
+End Class
