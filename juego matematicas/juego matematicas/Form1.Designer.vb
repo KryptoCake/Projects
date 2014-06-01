cadb75f5336a5551939ad488e182bd178768e749
Alex Try <tryescas@gmail.com>
2014-05-31 18:25:47 -0600

    mas avances
    arreglada la caratula. agregados niveles y sprints

--- juego matematicas/juego matematicas/Form1.Designer.vb
+++ juego matematicas/juego matematicas/Form1.Designer.vb
@@ -25,7 +25,6 @@ Partial Class Form1
         Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.lblX = New System.Windows.Forms.Label()
-        Me.MaskedTextBox1 = New System.Windows.Forms.MaskedTextBox()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
@@ -33,7 +32,6 @@ Partial Class Form1
         Me.LblTiempo = New System.Windows.Forms.Label()
         Me.LblPuntos = New System.Windows.Forms.Label()
         Me.Button5 = New System.Windows.Forms.Button()
-        Me.Label3 = New System.Windows.Forms.Label()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.LblY = New System.Windows.Forms.Label()
         Me.LblSigno = New System.Windows.Forms.Label()
@@ -41,39 +39,34 @@ Partial Class Form1
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Button6 = New System.Windows.Forms.Button()
         Me.LblTiempoAbs = New System.Windows.Forms.Label()
+        Me.CmbSprint = New System.Windows.Forms.ComboBox()
+        Me.TextBoxRes = New System.Windows.Forms.TextBox()
+        Me.CmbNivel = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'lblX
         '
-        Me.lblX.AutoSize = True
-        Me.lblX.Font = New System.Drawing.Font("Berlin Sans FB", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblX.Location = New System.Drawing.Point(232, 128)
+        Me.lblX.BackColor = System.Drawing.Color.Transparent
+        Me.lblX.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.lblX.Font = New System.Drawing.Font("Arial Narrow", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblX.Location = New System.Drawing.Point(251, 111)
         Me.lblX.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.lblX.Name = "lblX"
-        Me.lblX.Size = New System.Drawing.Size(106, 71)
+        Me.lblX.Size = New System.Drawing.Size(300, 75)
         Me.lblX.TabIndex = 0
         Me.lblX.Text = "00"
-        '
-        'MaskedTextBox1
-        '
-        Me.MaskedTextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 32.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.MaskedTextBox1.Location = New System.Drawing.Point(541, 144)
-        Me.MaskedTextBox1.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
-        Me.MaskedTextBox1.Mask = "99999"
-        Me.MaskedTextBox1.Name = "MaskedTextBox1"
-        Me.MaskedTextBox1.Size = New System.Drawing.Size(178, 56)
-        Me.MaskedTextBox1.TabIndex = 1
-        Me.MaskedTextBox1.ValidatingType = GetType(Integer)
+        Me.lblX.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         '
         'Button1
         '
         Me.Button1.BackColor = System.Drawing.Color.Cornsilk
         Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 32.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button1.ForeColor = System.Drawing.Color.CornflowerBlue
-        Me.Button1.Location = New System.Drawing.Point(88, 35)
+        Me.Button1.Location = New System.Drawing.Point(29, 9)
         Me.Button1.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(82, 60)
+        Me.Button1.Size = New System.Drawing.Size(46, 63)
         Me.Button1.TabIndex = 2
         Me.Button1.Text = "+"
         Me.Button1.UseVisualStyleBackColor = False
@@ -83,10 +76,10 @@ Partial Class Form1
         Me.Button2.BackColor = System.Drawing.Color.Cornsilk
         Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 32.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button2.ForeColor = System.Drawing.SystemColors.Highlight
-        Me.Button2.Location = New System.Drawing.Point(88, 96)
+        Me.Button2.Location = New System.Drawing.Point(29, 84)
         Me.Button2.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(82, 60)
+        Me.Button2.Size = New System.Drawing.Size(46, 63)
         Me.Button2.TabIndex = 3
         Me.Button2.Text = "-"
         Me.Button2.UseVisualStyleBackColor = False
@@ -96,10 +89,10 @@ Partial Class Form1
         Me.Button3.BackColor = System.Drawing.Color.Cornsilk
         Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 32.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button3.ForeColor = System.Drawing.SystemColors.Highlight
-        Me.Button3.Location = New System.Drawing.Point(88, 165)
+        Me.Button3.Location = New System.Drawing.Point(29, 153)
         Me.Button3.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(82, 61)
+        Me.Button3.Size = New System.Drawing.Size(46, 63)
         Me.Button3.TabIndex = 4
         Me.Button3.Text = "*"
         Me.Button3.UseVisualStyleBackColor = False
@@ -109,10 +102,10 @@ Partial Class Form1
         Me.Button4.BackColor = System.Drawing.Color.Cornsilk
         Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 32.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button4.ForeColor = System.Drawing.SystemColors.Highlight
-        Me.Button4.Location = New System.Drawing.Point(88, 234)
+        Me.Button4.Location = New System.Drawing.Point(29, 222)
         Me.Button4.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
         Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(82, 61)
+        Me.Button4.Size = New System.Drawing.Size(46, 73)
         Me.Button4.TabIndex = 5
         Me.Button4.Text = "/"
         Me.Button4.UseVisualStyleBackColor = False
@@ -120,8 +113,10 @@ Partial Class Form1
         'LblTiempo
         '
         Me.LblTiempo.AutoSize = True
+        Me.LblTiempo.BackColor = System.Drawing.Color.Transparent
         Me.LblTiempo.Font = New System.Drawing.Font("Microsoft Sans Serif", 52.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.LblTiempo.Location = New System.Drawing.Point(355, 9)
+        Me.LblTiempo.ForeColor = System.Drawing.Color.Blue
+        Me.LblTiempo.Location = New System.Drawing.Point(443, 12)
         Me.LblTiempo.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.LblTiempo.Name = "LblTiempo"
         Me.LblTiempo.Size = New System.Drawing.Size(73, 79)
@@ -131,6 +126,7 @@ Partial Class Form1
         'LblPuntos
         '
         Me.LblPuntos.AutoSize = True
+        Me.LblPuntos.BackColor = System.Drawing.Color.Transparent
         Me.LblPuntos.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.LblPuntos.Location = New System.Drawing.Point(572, 35)
         Me.LblPuntos.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
@@ -144,25 +140,13 @@ Partial Class Form1
         Me.Button5.BackColor = System.Drawing.Color.Cornsilk
         Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button5.ForeColor = System.Drawing.Color.CornflowerBlue
-        Me.Button5.Location = New System.Drawing.Point(261, 251)
+        Me.Button5.Location = New System.Drawing.Point(83, 320)
         Me.Button5.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
         Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(255, 46)
+        Me.Button5.Size = New System.Drawing.Size(122, 46)
         Me.Button5.TabIndex = 8
         Me.Button5.Text = "iniciar"
         Me.Button5.UseVisualStyleBackColor = False
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Wingdings", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(2, Byte))
-        Me.Label3.ForeColor = System.Drawing.SystemColors.HotTrack
-        Me.Label3.Location = New System.Drawing.Point(455, 23)
-        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(69, 71)
-        Me.Label3.TabIndex = 9
-        Me.Label3.Text = "6"
         '
         'Timer1
         '
@@ -170,20 +154,24 @@ Partial Class Form1
         '
         'LblY
         '
-        Me.LblY.AutoSize = True
-        Me.LblY.Font = New System.Drawing.Font("Berlin Sans FB", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.LblY.Location = New System.Drawing.Point(410, 129)
+        Me.LblY.AccessibleRole = System.Windows.Forms.AccessibleRole.None
+        Me.LblY.BackColor = System.Drawing.Color.Transparent
+        Me.LblY.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.LblY.Font = New System.Drawing.Font("Arial Narrow", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblY.Location = New System.Drawing.Point(267, 174)
         Me.LblY.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.LblY.Name = "LblY"
-        Me.LblY.Size = New System.Drawing.Size(106, 71)
+        Me.LblY.Size = New System.Drawing.Size(284, 71)
         Me.LblY.TabIndex = 10
         Me.LblY.Text = "00"
+        Me.LblY.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         '
         'LblSigno
         '
         Me.LblSigno.AutoSize = True
+        Me.LblSigno.BackColor = System.Drawing.Color.Transparent
         Me.LblSigno.Font = New System.Drawing.Font("Arial Narrow", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.LblSigno.Location = New System.Drawing.Point(342, 129)
+        Me.LblSigno.Location = New System.Drawing.Point(212, 162)
         Me.LblSigno.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.LblSigno.Name = "LblSigno"
         Me.LblSigno.Size = New System.Drawing.Size(63, 75)
@@ -193,6 +181,7 @@ Partial Class Form1
         'LblSumaPuntos
         '
         Me.LblSumaPuntos.AutoSize = True
+        Me.LblSumaPuntos.BackColor = System.Drawing.Color.Transparent
         Me.LblSumaPuntos.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.LblSumaPuntos.Location = New System.Drawing.Point(572, 84)
         Me.LblSumaPuntos.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
@@ -204,6 +193,7 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.Location = New System.Drawing.Point(753, 35)
         Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
@@ -217,10 +207,10 @@ Partial Class Form1
         Me.Button6.BackColor = System.Drawing.Color.Cornsilk
         Me.Button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button6.ForeColor = System.Drawing.Color.CornflowerBlue
-        Me.Button6.Location = New System.Drawing.Point(524, 249)
+        Me.Button6.Location = New System.Drawing.Point(235, 320)
         Me.Button6.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
         Me.Button6.Name = "Button6"
-        Me.Button6.Size = New System.Drawing.Size(255, 46)
+        Me.Button6.Size = New System.Drawing.Size(122, 46)
         Me.Button6.TabIndex = 14
         Me.Button6.Text = "Parar"
         Me.Button6.UseVisualStyleBackColor = False
@@ -228,13 +218,58 @@ Partial Class Form1
         'LblTiempoAbs
         '
         Me.LblTiempoAbs.AutoSize = True
+        Me.LblTiempoAbs.BackColor = System.Drawing.Color.Transparent
         Me.LblTiempoAbs.Font = New System.Drawing.Font("Microsoft Sans Serif", 52.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.LblTiempoAbs.Location = New System.Drawing.Point(216, 9)
+        Me.LblTiempoAbs.ForeColor = System.Drawing.Color.Blue
+        Me.LblTiempoAbs.Location = New System.Drawing.Point(284, 9)
         Me.LblTiempoAbs.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.LblTiempoAbs.Name = "LblTiempoAbs"
         Me.LblTiempoAbs.Size = New System.Drawing.Size(73, 79)
         Me.LblTiempoAbs.TabIndex = 15
         Me.LblTiempoAbs.Text = "0"
+        '
+        'CmbSprint
+        '
+        Me.CmbSprint.Font = New System.Drawing.Font("Arial", 8.25!)
+        Me.CmbSprint.FormattingEnabled = True
+        Me.CmbSprint.Items.AddRange(New Object() {"30 segundos", "1 minuto", "2 minutos", "3 minutos", "4 minutos", "5 minutos", "Sin limite"})
+        Me.CmbSprint.Location = New System.Drawing.Point(83, 12)
+        Me.CmbSprint.Name = "CmbSprint"
+        Me.CmbSprint.Size = New System.Drawing.Size(147, 22)
+        Me.CmbSprint.TabIndex = 16
+        Me.CmbSprint.Text = "Sprint"
+        '
+        'TextBoxRes
+        '
+        Me.TextBoxRes.Font = New System.Drawing.Font("Arial Narrow", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBoxRes.Location = New System.Drawing.Point(380, 262)
+        Me.TextBoxRes.Name = "TextBoxRes"
+        Me.TextBoxRes.Size = New System.Drawing.Size(154, 35)
+        Me.TextBoxRes.TabIndex = 17
+        Me.TextBoxRes.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'CmbNivel
+        '
+        Me.CmbNivel.Font = New System.Drawing.Font("Arial", 8.25!)
+        Me.CmbNivel.FormattingEnabled = True
+        Me.CmbNivel.Items.AddRange(New Object() {"1-básico: 1-5", "2-fácil: 1-10", "3-intermedio facil:1-15", "4-intermedio regular:1-20", "5-intermedio fuerte:1-25", "6-complejo sencillo:1-30", "7-complejo regular:10-35", "8-complejo complejo:10-50", "9-complejo extendido:10-70", "10-ligas mayores: 10-1000", "11-extremo:100-5000", "12-Elegir intervalo"})
+        Me.CmbNivel.Location = New System.Drawing.Point(83, 84)
+        Me.CmbNivel.Name = "CmbNivel"
+        Me.CmbNivel.Size = New System.Drawing.Size(147, 22)
+        Me.CmbNivel.TabIndex = 18
+        Me.CmbNivel.Text = "Nivel"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(327, 222)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(233, 37)
+        Me.Label2.TabIndex = 19
+        Me.Label2.Text = "____________"
         '
         'Form1
         '
@@ -242,13 +277,16 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
         Me.ClientSize = New System.Drawing.Size(1156, 411)
+        Me.Controls.Add(Me.LblY)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.CmbNivel)
+        Me.Controls.Add(Me.TextBoxRes)
+        Me.Controls.Add(Me.CmbSprint)
         Me.Controls.Add(Me.LblTiempoAbs)
         Me.Controls.Add(Me.Button6)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.LblSumaPuntos)
         Me.Controls.Add(Me.LblSigno)
-        Me.Controls.Add(Me.LblY)
-        Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Button5)
         Me.Controls.Add(Me.LblPuntos)
         Me.Controls.Add(Me.LblTiempo)
@@ -256,7 +294,6 @@ Partial Class Form1
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.MaskedTextBox1)
         Me.Controls.Add(Me.lblX)
         Me.Font = New System.Drawing.Font("Wingdings 2", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
@@ -267,7 +304,6 @@ Partial Class Form1
 
     End Sub
     Friend WithEvents lblX As System.Windows.Forms.Label
-    Friend WithEvents MaskedTextBox1 As System.Windows.Forms.MaskedTextBox
     Friend WithEvents Button1 As System.Windows.Forms.Button
     Friend WithEvents Button2 As System.Windows.Forms.Button
     Friend WithEvents Button3 As System.Windows.Forms.Button
@@ -275,7 +311,6 @@ Partial Class Form1
     Friend WithEvents LblTiempo As System.Windows.Forms.Label
     Friend WithEvents LblPuntos As System.Windows.Forms.Label
     Friend WithEvents Button5 As System.Windows.Forms.Button
-    Friend WithEvents Label3 As System.Windows.Forms.Label
     Friend WithEvents Timer1 As System.Windows.Forms.Timer
     Friend WithEvents LblY As System.Windows.Forms.Label
     Friend WithEvents LblSigno As System.Windows.Forms.Label
@@ -283,5 +318,9 @@ Partial Class Form1
     Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents Button6 As System.Windows.Forms.Button
     Friend WithEvents LblTiempoAbs As System.Windows.Forms.Label
+    Friend WithEvents CmbSprint As System.Windows.Forms.ComboBox
+    Friend WithEvents TextBoxRes As System.Windows.Forms.TextBox
+    Friend WithEvents CmbNivel As System.Windows.Forms.ComboBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
 
 End Class
--- juego matematicas/juego matematicas/Form1.vb
+++ juego matematicas/juego matematicas/Form1.vb
@@ -1,16 +1,45 @@
 ﻿Public Class Form1
 
-    Private Sub Label3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label3.Click
-
-    End Sub
-
     Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
         Dim i, j As Integer
+        Select CmbSprint.SelectedIndex
+            Case 0
+                If LblTiempoAbs.Text = "30" Then
+                    Timer1.Enabled = False
+                    TextBoxRes.Enabled = False
+                End If
+            Case 2
+                If LblTiempoAbs.Text = "60" Then
+                    Timer1.Enabled = False
+                    TextBoxRes.Enabled = False
+                End If
+            Case 3
+                If LblTiempoAbs.Text = "120" Then
+                    Timer1.Enabled = False
+                    TextBoxRes.Enabled = False
+                End If
+            Case 4
+                If LblTiempoAbs.Text = "180" Then
+                    Timer1.Enabled = False
+                    TextBoxRes.Enabled = False
+                End If
+            Case 5
+                If LblTiempoAbs.Text = "240" Then
+                    Timer1.Enabled = False
+                    TextBoxRes.Enabled = False
+                End If
+            Case 6
+                If LblTiempoAbs.Text = "320" Then
+                    Timer1.Enabled = False
+                    TextBoxRes.Enabled = False
+                End If
+            Case Else
+                If LblTiempoAbs.Text = "30" Then
+                    Timer1.Enabled = False
+                    TextBoxRes.Enabled = False
+                End If
+        End Select
 
-        If LblTiempoAbs.Text = "59" Then
-            Timer1.Enabled = False
-            MaskedTextBox1.Enabled = False
-        End If
         i = CInt(LblTiempo.Text)
         LblTiempo.Text = CStr(i + 1)
         j = CInt(LblTiempoAbs.Text)
@@ -19,25 +48,56 @@
 
     Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
         Timer1.Enabled = True
-        MaskedTextBox1.Enabled = True
+        TextBoxRes.Enabled = True
+        Dim numero, numerox As Integer
         Dim Random As New Random()
-
         ' generar un random  
-        Dim numero As Integer = Random.Next(1, 10)
-        Dim numerox As Integer = Random.Next(1, 10)
+        Select Case CmbNivel.SelectedIndex
+            Case 0
+                numero = Random.Next(1, 5)
+                numerox = Random.Next(1, 5)
+            Case 1
+                numero = Random.Next(1, 10)
+                numerox = Random.Next(1, 10)
+            Case 2
+                numero = Random.Next(1, 15)
+                numerox = Random.Next(1, 15)
+            Case 3
+                numero = Random.Next(1, 20)
+                numerox = Random.Next(1, 20)
+            Case 4
+                numero = Random.Next(1, 25)
+                numerox = Random.Next(1, 25)
+            Case 5
+                numero = Random.Next(1, 30)
+                numerox = Random.Next(1, 30)
+            Case 6
+                numero = Random.Next(10, 35)
+                numerox = Random.Next(10, 35)
+            Case 7
+                numero = Random.Next(10, 50)
+                numerox = Random.Next(10, 50)
+            Case 8
+                numero = Random.Next(10, 70)
+                numerox = Random.Next(10, 70)
+            Case 9
+                numero = Random.Next(10, 1000)
+                numerox = Random.Next(10, 1000)
+            Case 10
+                numero = Random.Next(10, 5000)
+                numerox = Random.Next(10, 5000)
+            Case 11
 
+            Case Else
+                numero = Random.Next(1, 5)
+                numerox = Random.Next(1, 5)
+        End Select
+
+        
         lblX.Text = CStr(numero)
 
         LblY.Text = CStr(numerox)
-        MaskedTextBox1.Focus()
-    End Sub
-
-    Private Sub Button5_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles Button5.KeyPress
-
-        'If e.KeyChar =  Then
-        'End If
-        'End If
-        'Timer1.Enabled = True
+        TextBoxRes.Focus()
     End Sub
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
@@ -56,7 +116,12 @@
         LblSigno.Text = "/"
     End Sub
 
-    Private Sub MaskedTextBox1_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MaskedTextBox1.KeyDown
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        Timer1.Enabled = False
+        LblTiempo.Text = "1"
+    End Sub
+
+    Private Sub TextBoxRes_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles TextBoxRes.KeyDown
         Dim resultado As Decimal
         Dim A As Char
         If e.KeyCode = Keys.Enter Then
@@ -73,29 +138,22 @@
                     resultado = CInt(lblX.Text) / CInt(LblY.Text)
                 Case Else
             End Select
-            If (MaskedTextBox1.Text <> "") Then
+            If (TextBoxRes.Text <> "") Then
 
-                If CInt(MaskedTextBox1.Text) = resultado Then
+                If CInt(TextBoxRes.Text) = resultado Then
 
-                    MaskedTextBox1.Clear()
-                    MaskedTextBox1.Focus()
+                    TextBoxRes.Clear()
+                    TextBoxRes.Focus()
                     LblPuntos.Text = CStr(resultado / CStr(LblTiempo.Text))
                     LblSumaPuntos.Text = LblSumaPuntos.Text + CDbl(LblPuntos.Text)
                     LblTiempo.Text = "1"
-                    
+
                     Call Button5_Click(sender, e)
 
                 End If
             End If
         End If
-
-
     End Sub
 
     
-
-    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
-        Timer1.Enabled = False
-        LblTiempo.Text = "1"
-    End Sub
 End Class
